181545d5f2c2c141734f1b8e45071a54ccb326fc
Darolyn55 <Darolynjohnson@gmail.com>
2025-04-21 12:41:57 -0400

    Uploaded mu calculator app

---
+++ Myday3JavaCalculatorProgram.java
@@ -0,0 +1,22 @@
+package day3;
+
+public class Myday3JavaCalculatorProgram {
+
+	public static void main(String[] args) {
+		/*
+		 * int num1=10; int num2=30; int num3=30; int num4=30; int sum;
+		 * sum=num1+num2+num3+num4; System.out.println(sum);
+		 */
+	
+	int num1=50;
+	int num2=50;
+	int num3=25;
+	int sum;
+	sum=num1*num2*num3;
+	System.out.println(sum);
+	
+	
+
+	}
+
+}
